c06e7498c315c401ec74424ccbf8bacaf41f0b71
anubra266 <anubra266@gmail.com>
2024-09-09 18:56:37 -0500

    init

---
+++ app/utils.ts
@@ -0,0 +1,10 @@
+const imageMap: Record<string, string> = {
+  panda: "https://panda-css.com/android-chrome-512x512.png",
+  zag: "https://zagjs.com/favicon/android-chrome-256x256.png",
+};
+
+export function getImage(data: any) {
+  const [org, project] = data.repo.split("/");
+  if (org === "chakra-ui" && project in imageMap) return imageMap[project];
+  else return `https://github.com/${data.repo.split("/")[0]}.png`;
+}
